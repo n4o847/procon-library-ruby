f9e7ed416b2bd0ec7664069b3e611fbd58e51f1a
n4o847 <yuu.miu.0925@gmail.com>
2018-10-21 15:34:06 +0900

    Feat コードの切り出し

--- lib/sum.rb
+++ lib/sum.rb
@@ -1,5 +1,9 @@
+# BEGIN
+
 def sum(a)
   c = *0...a.size
   s = a.reduce([0]) {|s,x| s << s[-1] + x }
   -> *x { (r = c[*x]) && (r.empty? ? 0 : s[r.last + 1] - s[r.first]) }
 end
+
+# END
--- use.rb
+++ use.rb
@@ -8,8 +8,11 @@ libs = {
 MODULE_NAME = "N4"
 
 puts [
+  "# https://github.com/n4o847/procon-library-ruby",
   "module #{MODULE_NAME}",
   "module_function",
-  libs.values_at(*ARGV).uniq.compact.map {|f| File.read(f).chomp.gsub(/\n\s*/, "; ") },
+  libs.values_at(*ARGV).uniq.compact.map {|f|
+    File.read(f)[/(?<=# BEGIN).*(?=# END)/m].strip.gsub(/\n\s*/, "; ")
+  },
   "end",
 ]
